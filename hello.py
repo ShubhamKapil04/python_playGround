3fc08988d208f7114c08e1c33dcd277afd12f82a
Shubham Kapil <skaplesh720@gmail.com>
2023-02-09 20:14:37 +0530

    Variables in python

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
 # Simple hello wolrd program 
+age = 20
+price = 19.50
+first_name = "Shubham"
+is_online = False
+print(is_online)
+print(first_name)
+print(price)
+print(age)
 print("Hello World")
