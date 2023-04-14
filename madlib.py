f666ac3d5701fa7b58b96779ab2381f590f3ebc7
Shubham Kapil <skaplesh720@gmail.com>
2023-04-14 21:58:53 +0530

    Python input

---
+++ madlib.py
@@ -0,0 +1,7 @@
+colour = input("Enter the Colour: ")
+plural_naun = input("Enter the Plural naun: ")
+celebrity = input("Enter the celebrity name: ")
+
+print("Roses are {colour}")
+print("{plural_naun} are blue")
+print("I love {celebrity}")
