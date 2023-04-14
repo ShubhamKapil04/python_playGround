32f60c6395e24bebf757a750f9f670868e8e38b1
Shubham Kapil <skaplesh720@gmail.com>
2023-04-14 22:11:07 +0530

    Lists in pyhton

---
+++ list.py
@@ -0,0 +1,8 @@
+friends = ["kartik", "shubham", "harsh", "karan", "sahil"]
+print(friends)
+print(friends[0])
+print(friends[-1])
+print(friends[1:4])
+##Modifying the Value
+friends[2] = "Kutta"
+print(friends)
