fa6732954a1086a394b4c9775bf2085351c5e6a4
Shubham Kapil <skaplesh720@gmail.com>
2023-06-06 21:46:53 +0530

    List and Function

---
+++ listExample.py
@@ -0,0 +1,9 @@
+# Duplicate values are allowed in List
+mylist = ["apple", "mango", "banana", "cherry", "dragonfruit", "apple1", "apple1"]
+
+for x in mylist:
+    print(x)
+
+print(len(mylist))
+
+# print(mylist)
