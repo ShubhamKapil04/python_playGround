9cdfd8e0534f06e9b5548a865a35fcd22cda8a74
Shubham Kapil <skaplesh720@gmail.com>
2023-08-07 18:24:43 +0530

    Opps in pyhton

---
+++ Oops/pythonoops.py
@@ -0,0 +1,15 @@
+
+
+# def map():
+#     material = 'house'
+#     print(f'I build {material}')
+
+class map():
+    material = 'House'
+    print(f'I build {material}')
+
+
+house = map() 
+house.material = 'newHouse'
+print(house.material)
+
